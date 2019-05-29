fa7d74e78f6a97e973dc1378af9111eb9f430962
Klayver Gontijo <klayvergontijo7@gmail.com>
2019-05-29 20:36:32 -0300

    Remoção de "[innerHTML]",  página de favoritos funcionando, adição de favoritos nos envios do cadastro

--- src/app/pages/favoritos/favoritos.page.ts
+++ src/app/pages/favoritos/favoritos.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
+import { ReceitaService, Receita } from 'src/service/receita.service';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-favoritos',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./favoritos.page.scss'],
 })
 export class FavoritosPage implements OnInit {
+  receitas: Receita[];
+  favoritos: string[];
+  show: any[];
 
-  constructor() { }
+  constructor(private receitaService: ReceitaService, private db: AngularFirestore){}
+  
+  private subscription;
+  private id: any;
 
-  ngOnInit() {
+  ngOnInit(){
+    this.show = [];
+    this.subscription = this.receitaService.getReceitas().subscribe(retorno => {
+      this.receitas = retorno;
+    });
+    firebase.auth().onAuthStateChanged((usuario) => {
+      if (usuario)
+        this.id = usuario.uid;
+      firebase.firestore().collection('usuarios').doc(this.id).get().then(resultado => {
+        if (resultado.data().favoritos){
+          this.favoritos = resultado.data().favoritos;
+          this.favoritos.map(f => this.db.collection('receitas').doc(f).get().subscribe(retorno => this.show = [...this.show, retorno.data()]));
+        }
+      })
+    });
   }
+  
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+ }
 
 }
